b35aeb2440490cb43d3549310e6d857529b2e5be
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-07-03 19:20:14 -0300

    feat(rOUTES): Adding the routes to v1 init, so they already are been use in the api

--- app/routes/v1/__init__.py
+++ app/routes/v1/__init__.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
 from app.routes.v1.auth_routes import router as auth_router
+from app.routes.v1.inventory_route import router as inventory_route
 from app.routes.v1.ping_route import router as ping_router
+from app.routes.v1.product_routes import router as product_router
 from app.routes.v1.users_routes import router as user_router
 
 routers = APIRouter(prefix="/v1")
-router_list = [auth_router, user_router, ping_router]
+router_list = [auth_router, inventory_route, product_router, user_router, ping_router]
 
 for router in router_list:
     # router.tags = routers.tags.append("v1")
