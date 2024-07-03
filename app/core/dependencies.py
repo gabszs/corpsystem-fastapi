0499c931691884f6b650bc53f4e570083398ee70
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-07-03 10:58:16 -0300

    refactor(Depts): Explcitin converting uuid id

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from uuid import UUID
 
 from fastapi import Depends
 from jose import jwt
@@ -29,7 +30,7 @@ async def get_current_user(token: str = Depends(JWTBearer()), service: UserServi
         token_data = Payload(**payload)
     except (jwt.JWTError, ValidationError):
         raise AuthError(detail="Could not validate credentials")
-    current_user: User = await service.get_by_id(token_data.id)  # type: ignore
+    current_user: User = await service.get_by_id(UUID(token_data.id))  # type: ignore
     if not current_user:
         raise AuthError(detail="User not found")
     return current_user
