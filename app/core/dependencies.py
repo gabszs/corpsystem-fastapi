dd973c23c435a9dfc720ad3eece339c089c02484
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-07-03 19:12:07 -0300

    feat(Depts): Adding new Annottated depts, also correcinting the imports to only use .service import

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -4,6 +4,7 @@ from uuid import UUID
 from fastapi import Depends
 from jose import jwt
 from pydantic import ValidationError
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
@@ -12,14 +13,18 @@ from app.core.exceptions import AuthError
 from app.core.security import JWTBearer
 from app.core.settings import settings
 from app.models import User
-from app.repository.user_repository import UserRepository
+from app.repository import InventoryRepository
+from app.repository import ProductRepository
+from app.repository import UserRepository
 from app.schemas.auth_schema import Payload
 from app.schemas.base_schema import FindBase
-from app.services.auth_service import AuthService
-from app.services.user_service import UserService
+from app.services import AuthService
+from app.services import InventoryService
+from app.services import ProductService
+from app.services import UserService
 
 
-async def get_user_service(session: Session = Depends(get_session_factory)):
+async def get_user_service(session: AsyncSession = Depends(get_session_factory)):
     user_repository = UserRepository(session_factory=session)
     return UserService(user_repository)
 
@@ -42,9 +47,19 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     return current_user
 
 
-async def get_auth_service(session: Session = Depends(get_session_factory)):
+async def get_auth_service(session: AsyncSession = Depends(get_session_factory)):
     user_repository = UserRepository(session_factory=session)
     return AuthService(user_repository=user_repository)
+
+
+async def get_product_service(session: AsyncSession = Depends(get_session_factory)):
+    product_repository = ProductRepository(session_factory=session)
+    return ProductService(product_repository)
+
+
+async def get_inventory_service(session: AsyncSession = Depends(get_session_factory)):
+    inventory_repository = InventoryRepository(session_factory=session)
+    return InventoryService(inventory_repository)
 
 
 FindQueryParameters = Annotated[FindBase, Depends()]
@@ -53,3 +68,5 @@ UserServiceDependency = Annotated[UserService, Depends(get_user_service)]
 CurrentUserDependency = Annotated[User, Depends(get_current_user)]
 AuthServiceDependency = Annotated[AuthService, Depends(get_auth_service)]
 CurrentActiveUserDependency = Annotated[User, Depends(get_current_active_user)]
+ProductServiceDependency = Annotated[ProductService, Depends(get_product_service)]
+InventoryServiceDependency = Annotated[InventoryService, Depends(get_inventory_service)]
