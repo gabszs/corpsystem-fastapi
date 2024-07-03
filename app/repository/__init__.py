ccb2c513be7f99bd86ec31498a8cdf8f1c17eb9c
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-07-03 19:14:48 -0300

    feat(Packages): Adding the dunder init to services and repository paclkages, to minimize the imports

---
+++ app/repository/__init__.py
@@ -0,0 +1,5 @@
+from .inventory_repository import InventoryRepository
+from .product_repository import ProductRepository
+from .user_repository import UserRepository
+
+__all__ = ["InventoryRepository", "ProductRepository", "UserRepository"]
---
+++ app/services/__init__.py
@@ -0,0 +1,6 @@
+from .auth_service import AuthService
+from .inventory_service import InventoryService
+from .product_service import ProductService
+from .user_service import UserService
+
+__all__ = ["AuthService", "InventoryService", "ProductService", "UserService"]
