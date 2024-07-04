436ae08a5842dd7ae105ef05b1b5734f124dd81f
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2024-07-04 18:41:12 -0300

    style(Format): Farmting

--- tests/mysql-permissions.sh
+++ tests/mysql-permissions.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "APPLYING PRIVILAGES"
-docker exec -it mysql /bin/bash
 MYSQL_ROOT_PASSWORD="app_password"  # Assuming you used this password in docker run
 
 mysql -u root -p$MYSQL_ROOT_PASSWORD
-
+#mysql -h 127.0.0.1 --port 8888 -u root -ppassword -e 'CREATE DATABASE IF NOT EXISTS test;'
 # Grant SELECT privilege on the specified table
 GRANT ALL PRIVILEGES ON test.* TO 'app_user'@'%';
 FLUSH PRIVILEGES;
